768f1b2a1f5ac471219056cbcf49c3f39e6c01f2
Daniel Curilla <curilladaniel@gmail.com>
2026-04-16 09:43:34 -0600

    cyoa app mostly working, few more tweaks left before complete

--- 03-adventure-game/main.go
+++ 03-adventure-game/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"html/template"
 	"log"
+	"net/http"
 	"os"
 )
 
@@ -19,9 +21,35 @@ type StoryArcOptions struct {
 	Arc  string `json:"arc"`
 }
 
+var tmpl = template.Must(template.ParseFiles("templates/story.html"))
+var storyArcs map[string]StoryArc
+
+func storyArcHandler(arcs map[string]StoryArc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		arc, ok := arcs[id]
+		if !ok {
+			http.NotFound(w, r)
+		}
+		err := tmpl.Execute(w, arc)
+		if err != nil {
+			http.Error(w, "internal server error", http.StatusInternalServerError)
+			log.Printf("template execute error: %s", err)
+		}
+	}
+}
+
 func main() {
-	var storyArcs map[string]StoryArc
-	b, err := os.ReadFile("gopher.json")
+	filepath := flag.String("file", "gopher.json", "filepath to story arc json file")
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "usage: %s <filepath>", os.Args[0])
+		fmt.Fprintf(os.Stderr, "Start a choose your own adventure storyline!\n")
+		fmt.Fprintf(os.Stderr, "options:\n")
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+
+	b, err := os.ReadFile(*filepath)
 	if err != nil {
 		log.Fatalf("could not read gopher.json: %v", err)
 	}
@@ -31,11 +59,15 @@ func main() {
 		log.Fatalf("could not parse gopher.json: %v", err)
 	}
 
-	tmpl := template.Must(template.ParseFiles("templates/story.html"))
-
-	err = tmpl.Execute(os.Stdout, storyArcs["intro"])
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/intro", http.StatusFound)
+	})
+	mux.HandleFunc("/{id}", storyArcHandler(storyArcs))
+	log.Printf("starting server on part 8080")
+	err = http.ListenAndServe(":8080", mux)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
 
 }
